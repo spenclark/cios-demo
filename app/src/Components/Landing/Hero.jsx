e6c8eabc851207014f314dcd4c282dec857a726e
Spencer <spenclark@hotmail.com>
2022-05-11 14:54:51 -0700

    Landing Render / Router / CSS Baseline

--- app/src/Components/Landing/Hero.jsx
+++ app/src/Components/Landing/Hero.jsx
@@ -3,7 +3,15 @@ import React from "react";
 function Hero() {
   return (
     <div>
+      <img src="../../logo.svg" />
       <h1>Hello Cios Team,</h1>
+      <p>
+        
+        This is a prototype I made ahead of my meeting. These are just
+    
+           representations of my ideas are not exaustive.{" "}
+     {" "}
+      </p>
     </div>
   );
 }
--- app/src/Components/Landing/TenantFlowCard.jsx
+++ app/src/Components/Landing/TenantFlowCard.jsx
@@ -1,17 +1,18 @@
-{
-  /* <ReactTinyLink
-  cardSize="small"
-  showGraphic={true}
-  maxLine={2}
-  minLine={1}
-  url="https://www.amazon.com/Steve-Madden-Mens-Jagwar-10-5/dp/B016X44MKA/ref=lp_18637582011_1_1?srs=18637582011&ie=UTF8&qid=1550721409&sr=8-1"
-/>; */
-}
-
 import React from "react";
+import ReactTinyLink from "react-tiny-link";
 
 function TenantFlowCard() {
-  return <div></div>;
+  return (
+    <div>
+      <ReactTinyLink
+        cardSize="small"
+        showGraphic={true}
+        maxLine={2}
+        minLine={1}
+        url="https://www.amazon.com/Steve-Madden-Mens-Jagwar-10-5/dp/B016X44MKA/ref=lp_18637582011_1_1?srs=18637582011&ie=UTF8&qid=1550721409&sr=8-1"
+      />
+    </div>
+  );
 }
 
 export default TenantFlowCard;
