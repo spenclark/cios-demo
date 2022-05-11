4bf425e0290f1cb1f7f4994a987c48a8b5b79a95
Spencer <spenclark@hotmail.com>
2022-05-11 14:37:29 -0700

    favicon assets

---
+++ app/src/Components/Landing/TenantFlowCard.jsx
@@ -0,0 +1,9 @@
+{
+  /* <ReactTinyLink
+  cardSize="small"
+  showGraphic={true}
+  maxLine={2}
+  minLine={1}
+  url="https://www.amazon.com/Steve-Madden-Mens-Jagwar-10-5/dp/B016X44MKA/ref=lp_18637582011_1_1?srs=18637582011&ie=UTF8&qid=1550721409&sr=8-1"
+/>; */
+}
