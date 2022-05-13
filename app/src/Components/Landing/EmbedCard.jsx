ecdb6eb356596b8930d8d5e8d8fd0a0981b46ab6
Spencer <spenclark@hotmail.com>
2022-05-13 13:15:32 -0700

    embedded

--- app/src/Components/Landing/EmbedCard.jsx
+++ app/src/Components/Landing/EmbedCard.jsx
@@ -12,6 +12,15 @@ function EmbedCard() {
       <TitleDiv>
         <ContentTitle>Recorded walk-through</ContentTitle>
         <ContentSubtitle></ContentSubtitle>
+        <iframe
+          width="620"
+          height="388"
+          src="https://www.loom.com/embed/e69d41f19f3340bdbd4ab8d9dc705526"
+          frameborder="0"
+          webkitallowfullscreen
+          mozallowfullscreen
+          allowfullscreen
+        ></iframe>
       </TitleDiv>
     </ContentCard>
   );
